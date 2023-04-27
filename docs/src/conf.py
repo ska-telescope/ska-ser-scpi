e22f8e8e020c3feb963090fa105e386b82b95f7f
Drew Devereux <drew.devereux@data61.csiro.au>
2023-04-27 01:30:25 +0000

    [LOW-464] docs

---
+++ docs/src/conf.py
@@ -0,0 +1,39 @@
+"""Configuration file for Sphinx."""
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../src'))
+
+def setup(app):
+    app.add_css_file('css/custom.css')
+
+project = 'ska-ser-scpi'
+copyright = '2023, SKAO'
+author = 'SKAO'
+release = '0.0.1'
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx_autodoc_typehints",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+html_context = {
+    "favicon": "img/favicon_mono.ico",
+    "logo": "img/logo.png",
+    "theme_logo_only": True,
+}
+
+# autodoc_mock_imports = [
+# ]
+
+intersphinx_mapping = {
+    "https://docs.python.org/3.10/": None,
+    "https://developer.skao.int/projects/ska-ser-devices/en/latest/": None,
+}
---
+++ docs/src/index.rst
@@ -0,0 +1,20 @@
+.. ska-ser-scpi documentation master file, created by
+   sphinx-quickstart on Wed Apr 26 12:28:59 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to ska-ser-scpi's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
