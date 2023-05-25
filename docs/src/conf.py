1f3b1df4e0b5d5f324fb669d0436f306dc62135d
Alan Chen <alan.chen@csiro.au>
2023-05-25 16:13:21 +0800

    Prepare to release v0.2.1

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -2,15 +2,17 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath("../../src"))
+
 
 def setup(app):
-    app.add_css_file('css/custom.css')
+    app.add_css_file("css/custom.css")
 
-project = 'ska-ser-scpi'
-copyright = '2023, SKAO'
-author = 'SKAO'
-release = '0.2.0'
+
+project = "ska-ser-scpi"
+copyright = "2023, SKAO"
+author = "SKAO"
+release = "0.2.1"
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -18,11 +20,11 @@ extensions = [
     "sphinx_autodoc_typehints",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
 html_context = {
     "favicon": "img/favicon_mono.ico",
--- src/ska_ser_scpi/__init__.py
+++ src/ska_ser_scpi/__init__.py
@@ -23,7 +23,7 @@ shows
 
 .. image:: scpi.png
 """
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 
 __all__ = [
